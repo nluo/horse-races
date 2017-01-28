91f24765bc422e97f1af3f8e8c3da911e51aa00b
Nick Luo <nick.luo@me.com>
2017-01-28 16:10:32 +0800

    Update RaceDetail Screen

--- app/components/views/raceDetail.tsx
+++ app/components/views/raceDetail.tsx
@@ -1,7 +1,39 @@
 import * as React from 'react'
 
-export function RaceDetail (props: any) {
+import { Card, CardActions, CardHeader, CardTitle, CardText } from 'material-ui/Card'
+
+import { ListItem } from 'material-ui/List'
+import Avatar from 'material-ui/Avatar'
+
+export function RaceDetail(props: any) {
+    const { race } = props
     return (
-        <h1> Race Detail View </h1>
+        <div>
+            <Card>
+                <CardHeader
+                    title="URL Avatar"
+                    subtitle="Subtitle"
+                    avatar="images/jsa-128.jpg"
+                />
+                <CardText>
+                    {
+                        race.competitors.map((competitor: any) => {
+                            return <ListItem
+                                key={competitor.eventId}
+                                primaryText={
+                                    <span> {competitor.name} </span>
+                                }
+                                leftAvatar={
+                                    <Avatar
+                                        size={30}
+                                    >
+                                        {competitor.saddleNumber}
+                                    </Avatar>
+                                } />
+                        })
+                    }
+                </CardText>
+            </Card>
+        </div>
     )
 }
