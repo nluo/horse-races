0691870ed129e9cb9870437c40bfd708df49a2a2
Nick Luo <nick.luo@me.com>
2017-02-13 07:27:53 +1000

    Initial implementation of redis cache

--- server/ts/service/retrieveData.ts
+++ server/ts/service/retrieveData.ts
@@ -4,15 +4,35 @@ import * as _ from 'lodash'
 const renameKeys = require('object-rename-keys')
 const baseBath = 'https://www.ladbrokes.com.au/api/actions'
 
+const redis = require('redis')
+const client = redis.createClient()
+
+
 export async function getNextRacesWithCompetitors(numberOfRaces: number) {
 
     const races = await getRaces(numberOfRaces)
-    return Promise.all(races.map(async (race: Race) => {
+
+    const raceKeys = await getRaceKeysFromCache()
+
+    const racesFromCache = await Promise.all(raceKeys.map(getRaceFromCache))
+
+    // console.log('Redis races is ', racesFromCache)
+    
+    if (racesFromCache.length >  0) {
+        console.log('races from cached!')
+        return racesFromCache
+    }
+    const racesWithCompetitors = await Promise.all(races.map(async (race: Race) => {
         let competitors = await getCompetitorsFromRace(race)
         return _.assign({}, race, {
             competitors: competitors
         })
     }))
+    await Promise.all(racesWithCompetitors.map((race: Race) => {
+        return setRaceIntoCache(race)
+    }))
+
+    return racesWithCompetitors
 }
 
 
@@ -31,6 +51,42 @@ function getCompetitorsFromRace(race: Race) {
         })
     })
 }
+
+function getRaceKeysFromCache(): Promise<[string]> {
+    return new Promise((resolve, reject) => {
+        client.keys('race:*', function (error, replies) {
+            console.log('error is ', error, ' replies is ', replies)
+            if (error) {
+                return reject(error)
+            }
+            return resolve(replies)
+        })
+    })
+}
+
+function getRaceFromCache (key: string) {
+    return new Promise((resolve, reject) => {
+        client.get(key, (error, response) => {
+            if (error) {
+                return reject(error)
+            }
+            console.log('the response from get is ', response)
+            return resolve(JSON.parse(response))
+        })
+    })
+}
+
+function setRaceIntoCache(race: Race) {
+    return new Promise((resolve, reject) => {
+        client.set(`race:${race.eventId}`, JSON.stringify(race), (error, results) => {
+            if (error) {
+                return reject(error)
+            }
+            return resolve(results)
+        })
+    }) 
+}
+
 
 function getRaces(numberOfRaces: number) {
     const url = `${baseBath}/update?feeds[next5]`
